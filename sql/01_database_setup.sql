141d9f8a397547b8eae361c69f611fd6d158eab1
Tiasha Ray <inboxtiasha@gmail.com>
2026-06-08 17:44:38 +0530

    Update 01_database_setup.sql

--- sql/01_database_setup.sql
+++ sql/01_database_setup.sql
@@ -1,12 +1,26 @@
 /*
-Create database for the project
-This database will store all tables related to the analysis
+Purpose:
+Create the database used for the Olist E-Commerce Data Analysis project.
+
+Description:
+This database stores all tables, relationships, and data required for
+analyzing customer behavior, orders, products, sellers, payments,
+reviews, and delivery performance from the Olist e-commerce dataset.
 */
 
 CREATE DATABASE olist_operations;
 USE olist_operations;
 
-
+CREATE TABLE orders (
+    order_id VARCHAR(50),
+    customer_id VARCHAR(50),
+    order_status VARCHAR(30),
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP
+);
 
 CREATE TABLE customers (
     customer_id VARCHAR(50),
@@ -14,6 +28,26 @@ CREATE TABLE customers (
     customer_zip_code_prefix VARCHAR(10),
     customer_city VARCHAR(100),
     customer_state CHAR(2)
+);
+
+CREATE TABLE order_reviews (
+    review_id VARCHAR(50),
+    order_id VARCHAR(50),
+    review_score INT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
+
+CREATE TABLE order_items (
+    order_id VARCHAR(50),
+    order_item_id INT,
+    product_id VARCHAR(50),
+    seller_id VARCHAR(50),
+    shipping_limit_date TIMESTAMP,
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2)
 );
 
 CREATE TABLE order_payments (
