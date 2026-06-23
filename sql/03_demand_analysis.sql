7d9ef36e060f5137cae04839d3fbfb25f6269679
Tiasha Ray <inboxtiasha@gmail.com>
2026-06-23 20:20:10 +0530

    Update 03_demand_analysis.sql

--- sql/03_demand_analysis.sql
+++ sql/03_demand_analysis.sql
@@ -11,3 +11,27 @@ GROUP BY
 ORDER BY
     order_year,
     order_month;
+
+--- Order Volume vs. Average Delivery Time Analysis
+
+SELECT
+    YEAR(order_purchase_timestamp) AS order_year,
+    MONTH(order_purchase_timestamp) AS order_month,
+    COUNT(*) AS total_orders,
+    ROUND(
+        AVG(
+            DATEDIFF(
+                order_delivered_customer_date,
+                order_purchase_timestamp
+            )
+        ),
+        2
+    ) AS avg_delivery_days
+FROM orders
+WHERE order_status = 'delivered'
+GROUP BY
+    YEAR(order_purchase_timestamp),
+    MONTH(order_purchase_timestamp)
+ORDER BY
+    order_year,
+    order_month;
