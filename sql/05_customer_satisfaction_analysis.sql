3f60a8be3d39129d44069b11f3237b9630898235
Tiasha Ray <inboxtiasha@gmail.com>
2026-06-23 20:12:46 +0530

    Update 05_customer_satisfaction_analysis.sql

--- sql/05_customer_satisfaction_analysis.sql
+++ sql/05_customer_satisfaction_analysis.sql
@@ -1,1 +1,16 @@
+--- On-Time vs Delayed Deliveries
 
+SELECT
+    CASE
+        WHEN order_delivered_customer_date <= order_estimated_delivery_date
+            THEN 'On Time'
+        ELSE 'Delayed'
+    END AS delivery_status,
+    COUNT(*) AS total_orders,
+    ROUND(AVG(review_score), 2) AS avg_review_score
+FROM orders o
+JOIN order_reviews r
+    ON o.order_id = r.order_id
+WHERE o.order_status = 'delivered'
+    AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY delivery_status;
