f92fda2160b27080a408b8351c08cea259a0a1b4
Tiasha Ray <inboxtiasha@gmail.com>
2026-06-23 07:37:55 +0530

    Update 03_demand_analysis.sql

--- sql/03_demand_analysis.sql
+++ sql/03_demand_analysis.sql
@@ -1,1 +1,11 @@
-
+SELECT
+    YEAR(order_purchase_timestamp) AS order_year,
+    MONTH(order_purchase_timestamp) AS order_month,
+    COUNT(*) AS total_orders
+FROM orders
+GROUP BY
+    YEAR(order_purchase_timestamp),
+    MONTH(order_purchase_timestamp)
+ORDER BY
+    order_year,
+    order_month;
